2d13ca5ac9bc76ed543e73c13ef25866d5aa8fd0
Damian Chee <silverousmechs@gmail.com>
2024-02-22 10:07:42 +0800

    Clean up functions in world.js

--- variables.js
+++ variables.js
@@ -21,3 +21,23 @@ const camera = {
 
 const body = document.querySelector("body");
 const containerElement = document.querySelector(".container");
+let playerElement;
+
+// isometric degrees
+var isoX = 60;
+var isoY = 0;
+var isoZ = 45;
+
+/**
+ *
+ * https://stackoverflow.com/questions/10506502/what-is-the-connection-between-an-isometric-angle-and-scale
+ * 45 * (Math.PI / 180) converts 45° into radians (π/4)
+ * cos(π/4) = sin(π/4) = 0.7071067811865476
+ * 1/cos(π/4) = 1/sin(π/4) = 1.414213562373095 and there's your magic number.
+ * I'm using X rotate 60 degrees, Z rotate 45 degrees
+ * as such, the magic number only works CLOSE enough to look like it works
+ */
+
+// See above for the magic number I'm using
+var isoXScale = ((world.tile + 6) * 1.414213562373095) / 2;
+var isoYScale = (world.tile + 3) / 1.414213562373095 / 2;
--- world.js
+++ world.js
@@ -1,127 +1,129 @@
-/**
- *
- * https://stackoverflow.com/questions/10506502/what-is-the-connection-between-an-isometric-angle-and-scale
- * 45 * (Math.PI / 180) converts 45° into radians (π/4)
- * cos(π/4) = sin(π/4) = 0.7071067811865476
- * 1/cos(π/4) = 1/sin(π/4) = 1.414213562373095 and there's your magic number.
- * I'm using X rotate 60 degrees, Y rotate 45 degrees
- * as such, the magic number only works CLOSE enough to look like it works
- */
+//
+// element = Element from querySelector
+// tx, ty, tz = TranslateX, TranslateY, TranslateZ
+// rx, ry, rz = RotateX, RotateY, RotateZ
+// WARNING: Function does NOT check whether element is empty or not
+//
+function moveIsometricElement(
+  isometric = false,
+  element,
+  tx = 0,
+  ty = 0,
+  tz = 0
+) {
+  if (isometric) {
+    element.style.transform = `
+      perspective(2000px) 
+      translateX(${tx}px) 
+      translateY(${ty}px) 
+      translateZ(${tz}px) 
+      rotateX(${isoX}deg) 
+      rotateY(${isoY}deg) 
+      rotateZ(${isoZ}deg)
+    `;
+  } else {
+    element.style.transform = `
+      perspective(2000px) 
+      translateX(${tx}px) 
+      translateY(${ty}px) 
+      translateZ(${tz}px)
+    `;
+  }
+}
 
 function moveNorth() {
-  // See above for the magic number I'm using
-  let newX = ((world.tile + 6) * 1.414213562373095) / 2;
-  let newY = (world.tile + 3) / 1.414213562373095 / 2;
+  camera.position.x -= isoXScale;
+  camera.position.y += isoYScale;
+  camera.position.z += isoXScale;
+  moveIsometricElement(
+    true,
+    containerElement,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z
+  );
 
-  camera.position.x -= newX;
-  camera.position.y += newY;
-  camera.position.z += newX;
-  containerElement.style.transform = `
-  perspective(2000px) 
-  translateX(${camera.position.x}px)
-  translateY(${camera.position.y}px)
-  translateZ(${camera.position.z}px)
-  rotateX(60deg)
-  rotateZ(45deg)
-  `;
-
-  const p = document.querySelector(".player");
-  player.position.x += 0;
+  // Northwards movement = negative Y-axis
   player.position.y -= 100 + 6;
-  player.position.z += 0;
-  p.style.transform = `
-  perspective(2000px) 
-  translateX(${player.position.x}px)
-  translateY(${player.position.y}px)
-  translateZ(${player.position.z}px)
-  `;
+  moveIsometricElement(
+    false,
+    playerElement,
+    player.position.x,
+    player.position.y,
+    player.position.z
+  );
 }
 
 function moveSouth() {
-  // See above for the magic number I'm using
-  let newX = ((world.tile + 6) * 1.414213562373095) / 2;
-  let newY = (world.tile + 3) / 1.414213562373095 / 2;
+  camera.position.x += isoXScale;
+  camera.position.y -= isoYScale;
+  camera.position.z -= isoXScale;
+  moveIsometricElement(
+    true,
+    containerElement,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z
+  );
 
-  camera.position.x += newX;
-  camera.position.y -= newY;
-  camera.position.z -= newX;
-  containerElement.style.transform = `
-  perspective(2000px) 
-  translateX(${camera.position.x}px)
-  translateY(${camera.position.y}px)
-  translateZ(${camera.position.z}px)
-  rotateX(60deg)
-  rotateZ(45deg)
-  `;
-
-  const p = document.querySelector(".player");
-  player.position.x += 0;
+  // Southwards movement = positive Y-axis
   player.position.y += 100 + 6;
-  player.position.z += 0;
-  p.style.transform = `
-  perspective(2000px) 
-  translateX(${player.position.x}px)
-  translateY(${player.position.y}px)
-  translateZ(${player.position.z}px)
-  `;
+  moveIsometricElement(
+    false,
+    playerElement,
+    player.position.x,
+    player.position.y,
+    player.position.z
+  );
 }
 
 function moveEast() {
-  // See above for the magic number I'm using
-  let newX = ((world.tile + 6) * 1.414213562373095) / 2;
-  let newY = (world.tile + 3) / 1.414213562373095 / 2;
+  camera.position.x -= isoXScale;
+  camera.position.y -= isoYScale;
+  camera.position.z -= isoXScale;
 
-  camera.position.x -= newX;
-  camera.position.y -= newY;
-  camera.position.z -= newX;
-  containerElement.style.transform = `
-  perspective(2000px) 
-  translateX(${camera.position.x}px)
-  translateY(${camera.position.y}px)
-  translateZ(${camera.position.z}px)
-  rotateX(60deg)
-  rotateZ(45deg)
-  `;
+  moveIsometricElement(
+    true,
+    containerElement,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z
+  );
 
-  const p = document.querySelector(".player");
+  // Eastward movement = positive X-axis
   player.position.x += 100 + 6;
-  player.position.y -= 0;
-  player.position.z += 0;
-  p.style.transform = `
-  perspective(2000px) 
-  translateX(${player.position.x}px)
-  translateY(${player.position.y}px)
-  translateZ(${player.position.z}px)
-  `;
+
+  moveIsometricElement(
+    false,
+    playerElement,
+    player.position.x,
+    player.position.y,
+    player.position.z
+  );
 }
 
 function moveWest() {
-  // See above for the magic number I'm using
-  let newX = ((world.tile + 6) * 1.414213562373095) / 2;
-  let newY = (world.tile + 3) / 1.414213562373095 / 2;
+  camera.position.x += isoXScale;
+  camera.position.y += isoYScale;
+  camera.position.z += isoXScale;
 
-  camera.position.x += newX;
-  camera.position.y += newY;
-  camera.position.z += newX;
-  containerElement.style.transform = `
-  perspective(2000px) 
-  translateX(${camera.position.x}px)
-  translateY(${camera.position.y}px)
-  translateZ(${camera.position.z}px)
-  rotateX(60deg)
-  rotateZ(45deg)
-  `;
+  moveIsometricElement(
+    true,
+    containerElement,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z
+  );
 
-  const p = document.querySelector(".player");
   player.position.x -= 100 + 6;
-  player.position.y -= 0;
-  player.position.z += 0;
-  p.style.transform = `
-  perspective(2000px) 
-  translateX(${player.position.x}px)
-  translateY(${player.position.y}px)
-  translateZ(${player.position.z}px)
-  `;
+
+  moveIsometricElement(
+    false,
+    playerElement,
+    player.position.x,
+    player.position.y,
+    player.position.z
+  );
 }
 
 function initializeWorldCoordinates() {
@@ -130,17 +132,29 @@ function initializeWorldCoordinates() {
   camera.position.y = 0;
   camera.position.z = 0;
 
-  containerElement.style.transform = `
-  perspective(2000px) 
-  translateX(${camera.position.x}px)
-  translateY(${camera.position.y}px)
-  translateZ(${camera.position.z}px)
-  rotateX(60deg)
-  rotateZ(45deg)
-  `;
+  moveIsometricElement(
+    true,
+    containerElement,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z
+  );
+
+  player.position.x = 0;
+  player.position.y = 0;
+  player.position.z = 0;
+
+  moveIsometricElement(
+    false,
+    playerElement,
+    player.position.x,
+    player.position.y,
+    player.position.z
+  );
 
   let adjustNorth = 0;
   let adjustWest = 0;
+
   // calculate move north how many times
   // check if row even or odd
   if (world.row % 2) {
@@ -159,30 +173,10 @@ function initializeWorldCoordinates() {
 
   for (let i = 0; i < adjustNorth; ++i) moveNorth();
   for (let i = 0; i < adjustWest; ++i) moveWest();
-
-  initializePlayerPosition();
-}
-
-function initializePlayerPosition() {
-  const playerX = (world.column * world.tile) / 2;
-  const playerY = (world.row * world.tile) / 2;
-  player.position.x = playerX;
-  player.position.y = playerY;
-
-  player.position.mapIndex =
-    Math.ceil(playerX / world.tile) + Math.ceil(playerY / world.tile);
-
-  containerElement.children[Math.ceil(playerX / world.tile) - 1].children[
-    Math.ceil(playerY / world.tile) - 1
-  ].classList.remove("square");
-
-  containerElement.children[Math.ceil(playerX / world.tile) - 1].children[
-    Math.ceil(playerY / world.tile) - 1
-  ].classList.add("player");
 }
 
 function createGrid() {
-  // Create/populate grid into the container
+  // Create/populate grid and player into the container
   for (let i = 0; i < world.column; ++i) {
     const col = document.createElement("div");
     col.className = "column";
@@ -195,35 +189,18 @@ function createGrid() {
       ro.style.height = `${world.tile}px`;
 
       col.appendChild(ro);
-      console.log(`Column: ${i} | Row: ${j}`);
     }
 
     containerElement.appendChild(col);
   }
 
-  // gridSize is the size of each grid, multiplied by the number of columns
-  // and rows dynamically. The 6 multiplied by column and row is to make space
-  // for padding, margins and borders
-  //
-  // Set the width and height for container for border shenanigans
-  // prettier-ignore
   containerElement.style.width = `
-  ${ world.tile * world.column + 6 * world.column }px`;
-
-  // prettier-ignore
+  ${world.tile * world.column + 6 * world.column}px`;
   containerElement.style.height = `
-  ${ world.tile * world.row + 6 * world.row }px`;
+  ${world.tile * world.row + 6 * world.row}px`;
 
-  // Create player also
-  {
-    const ro = document.createElement("div");
-    ro.classList.add("player");
-
-    ro.style.width = `${world.tile}px`;
-    ro.style.height = `${world.tile}px`;
-
-    containerElement.appendChild(ro);
-  }
+  // Create player, might be replaced later with "CreateEntity"
+  createPlayer();
 
   console.log("Created Grid");
 }
@@ -232,53 +209,20 @@ function deleteGrid() {
   while (containerElement.firstChild) {
     containerElement.removeChild(containerElement.firstChild);
   }
-  console.log("Deleted Grid");
+  if (!containerElement.firstChild) console.log("Deleted Grid");
+  else console.log("Error emptying grid");
 }
 
-// fill the world map based on which room we are in
-function initializeWorldMap(room) {
-  // copy over the map
-  world.map = [...room];
+function createPlayer() {
+  const newPlayer = document.createElement("div");
+  newPlayer.classList.add("player");
+  newPlayer.style.width = `${world.tile}px`;
+  newPlayer.style.height = `${world.tile}px`;
+  containerElement.appendChild(newPlayer);
 
-  const playerPosition = room.indexOf(9);
+  // assign to playerElement variable for use across the game
+  playerElement = newPlayer;
+
+  if (playerElement) console.log("Created Player");
+  else console.log("Error creating Player");
 }
-
-// check what's in the next space
-function move() {}
-
-//
-// OLD STUFF
-//
-// function initializeWorld() {
-//   world.size = world.column * world.row;
-
-//   // fill world.map based on size with empty space (1)
-//   for (let i = 0; i < world.size; ++i) world.map[i] = 1;
-//   console.log(world.map);
-
-//   // first row
-//   for (let i = 0; i < world.column; ++i) world.map[i] = 2;
-
-//   // left and right sides
-//   for (let i = 0; i < world.size; i += world.column) {
-//     console.log(`${i} ${i + world.column - 1}`);
-//     world.map[i] = 2;
-//     world.map[i + world.column - 1] = 2;
-//   }
-
-//   // last row
-//   for (let i = 1; i <= world.column; ++i) world.map[world.size - i] = 2;
-
-//   console.log(world.map);
-// }
-
-// function checkWall() {
-// find out which grid it's in
-// { positionX * width, positionY * height }
-//
-// check if current grid => next grid is wall
-// { nextX * width, nextY * height } === wall
-// return false
-// else move into that new position
-// return true
-// }
